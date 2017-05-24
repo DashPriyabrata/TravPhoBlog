c9b98dd8d8612a202181f2663f0568ce820dfd63
Priyabrata Dash <priyabrata.dash@autodesk.com>
2017-05-24 18:23:23 +0800

    PD [#19-2] Partial Instagram implementation in client side.

--- MyTravelBlog/Scripts/app/common/constants.js
+++ MyTravelBlog/Scripts/app/common/constants.js
@@ -2,4 +2,5 @@
             .constant('bloggerApiRoot', 'http://localhost:82/api/Blogger/')
             .constant('blogInfoApiRoot', 'http://localhost:82/api/BlogInfo/')
             .constant('categoryApiRoot', 'http://localhost:82/api/Category/')
-            .constant('userApiRoot', 'http://localhost:82/api/User/');
+            .constant('userApiRoot', 'http://localhost:82/api/User/')
+            .constant('socialConnectApiRoot', 'http://localhost:82/api/Social/');
---
+++ MyTravelBlog/Scripts/app/services/SocialConnectService.js
@@ -0,0 +1,19 @@
+﻿
+app.factory('socialConnectService', [
+    'socialConnectApiRoot', '$http', '$q', function (apiRoot, $http, $q) {
+        'use strict';
+        return {
+            apiRoot: apiRoot,
+            getInstagramRecentMedia: function () {
+                var def = $q.defer();
+
+                $http.get(apiRoot + 'Instagram/RecentMedia')
+                    .then(function (response) {
+                        def.resolve(response.data);
+                    }, def.reject);
+
+                return def.promise;
+            }
+        };
+    }
+])
