97e6bb7cc9ffb7a9a7b02754e65e33f19ac3c283
DashPriyabrata <pdash.job@gmail.com>
2017-08-29 02:30:59 +0800

    PD [#26] Header navbar css style fix + some optimization

---
+++ MyTravelBlog/Scripts/app/controllers/LayoutController.js
@@ -0,0 +1,8 @@
+﻿app.controller('LayoutController', ['$scope', '$location', LayoutController]);
+
+function LayoutController($scope, $location) {
+    'use strict';
+    $scope.isActive = function (viewLocation) {
+        return $location.$$absUrl.endsWith(viewLocation);
+    };
+}
